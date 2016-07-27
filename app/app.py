674592c199a98c5f2b27d55b0974db6e6616e6ba
johnlee1 <telljlee@gmail.com>
2016-07-27 10:07:33 -0400

    update

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import requests
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route('/gist')
+def get_data():
+    return requests.get('http://mock-server:3000/gists').content
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
